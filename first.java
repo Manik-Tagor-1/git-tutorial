b7b46d4801f865a189556ca5a3cb80b81821c5a3
Manik Roy Tagor <manikchandro.info@gmail.com>
2021-09-12 16:38:36 +0600

    Nice

---
+++ first.java
@@ -0,0 +1,11 @@
+class studentClass{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+
+public function greeting(){
+    System.out.println("My Name is Manik Roy Tagor");
+}
+
+}  
+
